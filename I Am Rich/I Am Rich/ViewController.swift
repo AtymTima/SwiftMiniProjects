c5c6ae9400b2310727dad01353a66dfdbc54d83c
AtymTima <timanis188@gmail.com>
2020-10-12 23:26:05 +0600

    Added new project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by  Тима on 10/12/20.
+//  Copyright © 2020 atymtima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
