2f08b22e7bf5186081b5a64e1b05990eb3ea0d60
AtymTima <timanis188@gmail.com>
2020-10-15 00:06:32 +0600

    Added project

---
+++ Dice Roll/Dice Roll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by  Тима on 10/14/20.
+//  Copyright © 2020 atymtima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var rollImageViewLeft: UIImageView!
+    @IBOutlet weak var rollImageViewRight: UIImageView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+    }
+    
+}
+
